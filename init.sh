b9cb33f65d6f94435e72f13af29c857952bf7b54
Murwen <MurwenBing@hotmail.com>
2016-04-20 13:32:48 +0800

    any reflection

--- init.sh
+++ init.sh
@@ -1,1 +1,23 @@
 #!/bin/sh
+
+DIRNAME=`pwd`
+
+render() {
+	
+	for file in `ls $1`
+	do
+		local currfile=$1"/"$file
+		if [ -d $currfile ];then
+			render $currfile	
+		else
+			if [ $currfile != $DIRNAME"/"${0##*/} ] && [ ${currfile##*.} == 'sh' ]
+			then
+				echo $currfile	
+			fi
+		fi
+	done
+
+
+}
+render $DIRNAME
+
---
+++ system/disk/README.md
@@ -0,0 +1,1 @@
+1:statistic and analysis for disk
